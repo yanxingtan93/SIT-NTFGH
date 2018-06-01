57dba67f7b3b29aa7c5d12f7eb0b59352f61731b
kokleongkl <l.kokleong1991@gmail.com>
2018-06-02 00:51:00 +0800

    Commit for db connection

---
+++ src/DBUtils/DBConn.java
@@ -0,0 +1,26 @@
+package DBUtils;
+
+import java.sql.*;
+
+
+public class DBConn {
+    public Connection dbConnection(){
+        String hostName = "itpgroup1.database.windows.net";
+        String dbName = "SIT-NGTFH";
+        String user = "dbadmin@itpgroup1";
+        String password = "dbPassword!";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+
+}
