c26d0543cfb3f385c8d2f108a8a206a498df06fb
kokleongkl <l.kokleong1991@gmail.com>
2018-06-05 01:14:26 +0800

    Commit for db connection

--- src/DBUtils/DBConn.java
+++ src/DBUtils/DBConn.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 
 public class DBConn {
-    public Connection dbConnection(){
+    /*public Connection dbConnection() {
         String hostName = "itpgroup1.database.windows.net";
         String dbName = "SIT-NGTFH";
         String user = "dbadmin@itpgroup1";
@@ -15,10 +15,34 @@ public class DBConn {
         try {
             connection = DriverManager.getConnection(url);
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return connection;
+    }*/
+    public static PreparedStatement getPreparedStatement(String sql) throws SQLException {
+        PreparedStatement ps = null;
+        String hostName = "itpgroup1.database.windows.net";
+        String dbName = "SIT-NGTFH";
+        String user = "dbadmin@itpgroup1";
+        String password = "dbPassword!";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = DriverManager.getConnection(url);
+        ps = connection.prepareStatement(sql);
+        return ps;
+
+
+
+    }
+    //check connections.
+    public static void main(String[]args) throws SQLException {
+        ResultSet resultSet= getPreparedStatement("Select * From drugs").executeQuery();
+        while (resultSet.next()){
+            System.out.print(resultSet.getString(1) + " " +resultSet.getString(2));
+            System.out.print(" ");
+        }
+
+
     }
 
 
