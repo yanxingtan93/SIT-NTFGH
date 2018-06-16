f65f7c4bad735962e1ccb21a19866f004671d267
kokleongkl <l.kokleong1991@gmail.com>
2018-06-16 22:46:20 +0800

    commit for upload content

---
+++ src/Servlets/contentServlet.java
@@ -0,0 +1,62 @@
+package Servlets;
+
+import java.io.File;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@WebServlet(name = "contentServlet")
+public class contentServlet extends HttpServlet {
+    private final String UPLOAD_DIRECTORY = "data";
+    private String uploded_directory="";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //process only if its multipart content
+        if(ServletFileUpload.isMultipartContent(request)){
+            try {
+                List<FileItem> multiparts = new ServletFileUpload(
+                        new DiskFileItemFactory()).parseRequest(request);
+                String uploadPath = getServletContext().getRealPath("")
+                +UPLOAD_DIRECTORY;
+                File uploadDir = new File(uploadPath);
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+
+
+                for(FileItem item : multiparts){
+                    if(!item.isFormField()){
+                        String name = new File(item.getName()).getName();
+                        item.write( new File(uploadPath + File.separator + name));
+                        uploded_directory= uploadPath+ File.separator+name;
+                        System.out.println(uploded_directory);
+
+                    }
+                }
+
+                //File uploaded successfully
+                request.setAttribute("message", "File Uploaded Successfully at "+uploded_directory);
+            } catch (Exception ex) {
+                request.setAttribute("message", "File Upload Failed due to " + ex);
+            }
+
+        }else{
+            request.setAttribute("message",
+                    "Sorry this Servlet only handles file upload request");
+        }
+
+        request.getRequestDispatcher("result.jsp").forward(request, response);
+
+    }
+}
